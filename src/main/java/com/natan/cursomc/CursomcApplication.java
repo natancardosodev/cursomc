76ab6a82f7bfd77cf77a5b8cd2224022e20450f2
Natan Cardoso <natancb7@gmail.com>
2019-01-21 11:23:15 -0300

    Estrutura inicial de projeto em Spring Boot

---
+++ src/main/java/com/natan/cursomc/CursomcApplication.java
@@ -0,0 +1,14 @@
+package com.natan.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
+
