661bb06b8c9829d8b289cace1e77b5fcb76ac4fa
Natan Cardoso <natancb7@gmail.com>
2019-01-21 11:54:22 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/natan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/natan/cursomc/resources/CategoriaResource.java
@@ -3,9 +3,14 @@
  */
 package com.natan.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.natan.cursomc.domain.Categoria;
 
 /**
  * @author NatanPC
@@ -16,8 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
