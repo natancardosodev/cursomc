9b76d481dea820038c34571a292760628d2e63d6
Natan Cardoso <natancb7@gmail.com>
2019-01-23 12:31:29 -0300

    Criando operação de instanciação

--- src/main/java/com/natan/cursomc/CursomcApplication.java
+++ src/main/java/com/natan/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.natan.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.natan.cursomc.domain.Categoria;
+import com.natan.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// instanciação de classes
+		// povoando o sistema ao iniciar 
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
